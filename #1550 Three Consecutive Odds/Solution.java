2c7e4e9558b92074f83d5e0007dbcaa90f7002e2
Abhiroop Yerramilli <77533389+crayonthree@users.noreply.github.com>
2025-02-19 22:44:42 -0400

    1550

---
+++ #1550 Three Consecutive Odds/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int j=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2 != 0 || arr[i] == 1){
+                j++;
+            }else{
+                j = 0;
+            }
+            if(j>2){
+                return true;
+            }
+        }
+        return false;
+    }
+}
