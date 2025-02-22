c7abc024ecb7dda647a39fee1bf01f55361433c0
Abhiroop Yerramilli <77533389+crayonthree@users.noreply.github.com>
2025-02-21 20:09:48 -0400

    58

---
+++ #58 Length of Last Word/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] tokens = s.split(" ");
+        return tokens[tokens.length - 1].length();
+        
+    }
+}
+/* Time Complexity : O(n) */
